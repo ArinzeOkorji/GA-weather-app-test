8f82afc86cf7a4f92a1f84781f643b77e5007738
ArinzeOkorji <arinzeokorji@yahoo.com>
2021-08-27 13:38:39 +0100

    fix: chanhe http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   APIKey: 'e381d93bb69185a7858e68db769e40cb',
-  api: "http://api.openweathermap.org/data/2.5/"
+  api: "https://api.openweathermap.org/data/2.5/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   APIKey: 'e381d93bb69185a7858e68db769e40cb',
-  api: "http://api.openweathermap.org/data/2.5/"
+  api: "https://api.openweathermap.org/data/2.5/"
 };
 
 /*
